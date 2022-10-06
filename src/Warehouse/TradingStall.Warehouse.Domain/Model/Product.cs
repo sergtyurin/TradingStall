70f9c2a0acd66d99f8d4741943c62df141816070
styurin <1@1.ru>
2022-10-06 10:55:20 +0300

    Domain objects described

---
+++ src/Warehouse/TradingStall.Warehouse.Domain/Model/Product.cs
@@ -0,0 +1,22 @@
+namespace TradingStall.Warehouse.Domain.Model;
+
+public class Product : IProduct
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public long BrandId { get; set; }
+    public Brand Brand { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; }
+    public int AvailableStock { get; private set; }
+
+    public int AddStock(int quantity) => AvailableStock += quantity;
+
+    public int RemoveStock(int quantity)
+    {
+        if (AvailableStock < quantity)
+            throw new Exception($"Insufficient quantity of {Name} in stock");
+
+        return AvailableStock -= quantity;
+    }
+}
